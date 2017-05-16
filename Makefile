40a57b9ede679b87434b3f759ec3867b9cd0238b
A Retha <a_retha@ymail.com>
2017-05-16 22:48:45 +0100

    Synthetic data experiments done

--- Makefile
+++ Makefile
@@ -1,12 +1,12 @@
 
 CC=     g++
 
-# -O3
-CFLAGS= -Wall -g -D_USE_64 -msse4.2 -funroll-loops -fomit-frame-pointer
+# -O3 -Wall -g 
+CFLAGS= -O3 -D_USE_64 -msse4.2 -funroll-loops -fomit-frame-pointer
 
 LFLAGS= -std=c++11 -DNDEBUG -lz -lm -lpthread -I . \
         -I ./sdsl-lite/include/ \
-		-L ./sdsl-lite/lib/ -lsdsl -ldivsufsort -ldivsufsort64 -Wl,-rpath=$(PWD)/sdsl-lite/lib \
+        -L ./sdsl-lite/lib/ -lsdsl -ldivsufsort -ldivsufsort64 -Wl,-rpath=$(PWD)/sdsl-lite/lib \
         -I ./vcflib/tabixpp/ -I ./vcflib/tabixpp/htslib/ -I ./vcflib/smithwaterman/ -I ./vcflib/multichoose/ -I ./vcflib/filevercmp/ -I ./vcflib/src/ \
         -L ./vcflib/ -L ./vcflib/tabixpp/htslib/ -lvcflib -lhts -Wl,-rpath=$(PWD)/vcflib/ -Wl,-rpath=$(PWD)/vcflib/tabixpp/htslib/
 
---
+++ experiments/syntheticdata/test.py
@@ -0,0 +1,62 @@
+
+import os
+import sys
+import random
+import argparse
+import subprocess
+
+#
+# Parse command line arguments and check input
+#
+parser = argparse.ArgumentParser(description='Test Multi-EDSM using randomly-generated synthetic data. Provide \
+a pattern size like 16 and a total length like 64 and it will create 4 random patterns with combined length 64 \
+and run Multi-EDSM on the provided sequence file.')
+parser.add_argument('pattern_size', type=int, help='the length of each randomly generated pattern, e.g. 16')
+parser.add_argument('total_size', type=int, help='How long the total length of the patterns will be, e.g. 64')
+parser.add_argument('sequence_file', type=argparse.FileType('r'), help='The path to the sequence file to search through')
+parser.add_argument('--recreate-files', action='store_true', help='Delete and recreate the pattern file')
+args = parser.parse_args()
+
+if args.total_size < args.pattern_size:
+	print 'Error: Pattern length cannot be larger than total length!'
+	sys.exit(1)
+elif args.total_size % args.pattern_size != 0:
+	print 'Error: Invalid total size. Pattern length must be a factor of the total size.'
+	sys.exit(1)
+
+sequenceFileName = os.path.basename(args.sequence_file.name)
+sequenceFileName = ".".join(sequenceFileName.split('.')[0:-1])
+patternsFile = './randomPatterns/' + str(args.pattern_size) + '_' + str(args.total_size) + '.txt'
+resultsFile = './results/' + str(args.pattern_size) + '_' + str(args.total_size) + '_' + sequenceFileName + '.txt'
+
+if args.recreate_files:
+	if os.path.is_file(patternsFile):
+		os.remove(patternsFile)
+	if os.path.is_file(resultsFile):
+		os.remove(resultsFile)
+
+#
+# Create the patterns file
+#
+alphabet = 'ACGT'
+sigma = len(alphabet)
+if not os.path.exists(patternsFile):
+	total = 0
+	with open(patternsFile, 'w') as f:
+		while total < args.total_size:
+			if total != 0:
+				f.write("\n")
+			for j in range(args.pattern_size):
+				f.write(alphabet[random.randint(0, sigma - 1)])
+			total += args.pattern_size
+
+#
+# Search for the patterns with Multi-EDSM
+#
+cmd = './../../multiedsm ' + args.sequence_file.name + ' ' + patternsFile
+output = subprocess.Popen(['time ' + cmd], shell=True, stderr=subprocess.STDOUT, stdout=subprocess.PIPE).communicate()[0]
+with open(resultsFile, 'w') as f:
+	f.write(output)
+
+print output
+
